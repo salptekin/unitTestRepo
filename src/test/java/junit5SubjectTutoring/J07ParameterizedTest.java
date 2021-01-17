d88e5ee77f0cd75ca61ebc666fc387b6d616b672
apple <apple@apples-imac-6.attlocal.net>
2021-01-17 16:22:26 -0500

    completed

--- src/test/java/junit5SubjectTutoring/J07ParameterizedTest.java
+++ src/test/java/junit5SubjectTutoring/J07ParameterizedTest.java
@@ -1,8 +1,10 @@
 package junit5SubjectTutoring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class J07ParameterizedTest {
@@ -27,5 +29,39 @@ public class J07ParameterizedTest {
 		assertTrue(str.length()>=0);
 	
 	}
+	
+	@Test
+	void upperCaseTest1() {
+		
+		String str1 = "Ali Can";
+		String str2 = "ali can";
+		String str3 = "ali";
+		String str4 = "123";
+		String str5 = "A";
+		String str6 = "";
+		
+		assertEquals("ALI CAN", str1.toUpperCase());
+		assertEquals("ALI CAN", str2.toUpperCase());
+		assertEquals("ALI", str3.toUpperCase());
+		assertEquals("123", str4.toUpperCase());
+		assertEquals("A", str5.toUpperCase());
+		assertEquals("", str6.toUpperCase());
+	}
+	
+	@ParameterizedTest
+	@CsvSource(value = {"ALI CAN, Ali Can", "ALI CAN, ali can", "ALI, ali", "123, 123", "A, A", "\"\",\"\""})
+	void upperCaseTest2(String s1, String s2) {
+		assertEquals(s1, s2.toUpperCase());
+	}
+	
+	//Check if a String is containing "a" or not.
+	//Use "ali" - "joe" - "tom"
+	//Use @CsvSource annotation
+	
+	@ParameterizedTest
+    @CsvSource(value= {"true, ali, a","false, joe, a", "false, tom, a"})
+    void containsTest(Boolean b1, String s1, String s2) {
+       assertEquals(b1, s1.contains(s2));
+    }
 
 }
